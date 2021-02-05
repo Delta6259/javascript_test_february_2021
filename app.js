ba0945cff45b9be42cabe0a3a88351dba75a2769
Guillaume TRUPIN <gtrupin@gmail.com>
2021-02-05 14:43:18 +0100

    feat: rework booleans.

--- app.js
+++ app.js
@@ -3,16 +3,15 @@
 const args = require('yargs').argv;
 const filterParamValue = args.filter
 const countParamIsPresent = args['count']
+const mustFilter = ![null, undefined].includes(filterParamValue)
+const mustCount =  ![null, undefined].includes(countParamIsPresent)
 const data = require('./data')
 
-if (!filterParamValue || countParamIsPresent) {
+if (!mustFilter && !mustCount) {
     console.log(data)
     console.log('No parameters given : the full data list is given.')
     return
 }
-
-const mustFilter = ![null, undefined].includes(filterParamValue)
-const mustCount =  ![null, undefined].includes(countParamIsPresent)
 
 if (mustFilter) {
     console.log(`Filter data from ${filterParamValue}.`)
